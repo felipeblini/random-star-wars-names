b71fd14e19891c7c5ce0943dad25cf10fd542655
Felipe blini <Felipe.blini@gmail.com>
2016-12-19 19:51:34 -0300

    Updated test. Add new tests

--- src/index.test.js
+++ src/index.test.js
@@ -4,7 +4,7 @@ var starwars = require('./index');
 
 describe('starwars-names',  () => {
     
-    describe('all', () => {
+    describe('method:all', () => {
         
         it('should be an array of strings', () => {
             expect(starwars.all).to.satisfy(isArrayOfString);
@@ -18,21 +18,24 @@ describe('starwars-names',  () => {
         it('should contain Luke Skywalker', () => {
             expect(starwars.all).to.include('Luke Skywalker');
         });
+
+        
+        it('should list more than 1 name...', () => {
+            expect(starwars.all.length).to.be.above(1);
+        });
+            
             
     });
 
     
-    describe('ramdon', () => {
+    describe('method:ramdon', () => {
         
         it('should return a random item from the starwars.all', () => {
             var randomItem = starwars.random();
             expect(starwars.all).to.include(randomItem);
-        });
-            
+        });    
     });
         
-        
-    
     it('it should works...', () => {
         expect(true).to.be.true;
     });
