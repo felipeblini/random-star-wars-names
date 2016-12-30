c23c446e630f775104db3d001947153c08abf82d
Felipe blini <Felipe.blini@gmail.com>
2016-12-30 02:39:25 -0300

    chore(ghooks): Added ghooks
    Addd ghooks to protected us to commit code with breaking tests

--- src/index.test.js
+++ src/index.test.js
@@ -6,7 +6,7 @@ describe('starwars-names',  function() {
     
     describe('method:all', function() {
         
-        it('should be an array of strings', function() {
+        it('Should be an array of strings', function() {
             expect(starwars.all).to.satisfy(isArrayOfString);
 
             function isArrayOfString(array) {
@@ -16,52 +16,59 @@ describe('starwars-names',  function() {
             }
         });
         
-        it('should contain Luke Skywalker', function() {
+        it('Should contain Luke Skywalker', function() {
             expect(starwars.all).to.include('Luke Skywalker');
         });
 
-        it('should list more than 1 name...', function() {
+        it('Should list more than 1 name...', function() {
             expect(starwars.all).to.have.length.above(1);
         });
     });
 
     describe('method:ramdon', function() {
         
-        it('should return a random item from the starwars.all method',
+        it('Should return a random item from the starwars.all method',
         function () {
             var randomItem = starwars.random();
             expect(starwars.all).to.include(randomItem);
         });  
 
-        it('it should return a array of random names if passed ' +
-            'a number and it must me included in the names list',
+        it('Should return an array of random names with size of the ' +
+            ' number passed as a parameter',
         function() {
             var randomItems = starwars.random(2);
             
             if(starwars.all.length >= 2) {
+                // if the list have at least 2 names, the returned
+                // array must have obligatorily 2 item
                 expect(randomItems).to.have.length(2);
             } else {
-                expect(randomItems).to.have.length.bellow(3);
+                expect(randomItems).to.have.length.bellow(2);
             }
-
-            randomItems.forEach(function (item) {
-                expect(starwars.all).to.include(item);
-            });
         });
 
-        it('it should return a array of 2 random names if ' +
-            'passed 2 and the list has 2 or more names', function() {
-            var randomItems = starwars.random(2);
+        it('Should return a array of 1 random name if passed 1 as a parameter' +
+            ' and the list have at least 1 name', function() {
+            var randomItems = starwars.random(1);
             
-            if(starwars.all.length >= 2) {
-                expect(randomItems).to.have.length(2);
+            if(starwars.all.length >= 1) {
+                expect(randomItems).to.have.length(1);
+            }
+        });
+
+        it('Should return a array of 5 random names if passed 2 as a parameter' +
+            ' and the list have at least 5 names', function() {
+            var randomItems = starwars.random(5);
+            
+            if(starwars.all.length >= 5) {
+                expect(randomItems).to.have.length(5);
             }
         });
     });
     
     describe('method:byName', function () {
         
-        it('should return all names that contains the substring ' +
+        it('Should return all names that contains the substring ' +
             'passed as a parameter', function() {
             expect(starwars.byName('Luke Skywalker'))
                 .to.contains('Luke Skywalker');
